f560502345bec0c0fd2e0a1ede8c6e5be6d06e3b
Rohan McLure <rohanmclure@gmail.com>
2020-01-30 14:30:02 -0800

    Template for unit annotation of methods

--- src/Utilities/UnitAnnotations/uaware.jl
+++ src/Utilities/UnitAnnotations/uaware.jl
@@ -1,6 +1,9 @@
 using MacroTools
 using MacroTools: postwalk, @capture, @expand, prettify
 
+using ..PlanetParameters
+using ..ParametersType: getval
+
 """
   Duplicate structure definitions, maintaining a common constructor, to enable optional
   unit annotation.
@@ -9,95 +12,126 @@ using MacroTools: postwalk, @capture, @expand, prettify
   may still be invoked on drivers which do not support units.
 """
 macro uaware(ex)
-  @capture(ex, struct sig_ attrs__ end) || error("@uaware must be applied to a struct definiton")
+  if @capture(ex, struct sig_ attrs__ end)
 
-  # Remove docstrings from attrs
-  attrs = filter(x->!(x isa String), attrs)
+    # Remove docstrings from attrs
+    attrs = filter(x->!(x isa String), attrs)
 
-  # Rename the struct
-  local sig_unitless, sig_unitful, name, union, p1, p2
-  if @capture(sig, name_{p1__} <: sname_{p2__})
-    lname = Symbol(:L, name)
-    uname = Symbol(:U, name)
-    sig_unitless = :($lname{$(p1...)} <: $sname{$(p2...)})
-    sig_unitful  = :($uname{$(p1...)} <: $sname{$(p2...)})
-    union = :($name{$(p1...)} = Union{$lname{$(p1...)}, $uname{$(p1...)}})
-  elseif @capture(sig, name_{p1__} <: sname_)
-    p2 = Any[]
-    lname = Symbol(:L, name)
-    uname = Symbol(:U, name)
-    sig_unitless = :($lname{$(p1...)} <: $sname)
-    sig_unitful  = :($uname{$(p1...)} <: $sname)
-    union = :($name{$(p1...)} = Union{$lname{$(p1...)}, $uname{$(p1...)}})
-  elseif @capture(sig, name_{p1__})
-    lname = Symbol(:L, name)
-    uname = Symbol(:U, name)
-    sig_unitless = :($lname{$(p1...)})
-    sig_unitful  = :($uname{$(p1...)})
-    union = :($name{$(p1...)} = Union{$lname{$(p1...)}, $uname{$(p1...)}})
-  elseif @capture(sig, name_)
-    lname = Symbol(:L, name)
-    uname = Symbol(:U, name)
-    p1, p2 = Any[], Any[]
-    sig_unitless = lname
-    sig_unitful  = uname
-    union = :($name = Union{$sig_unitless, $sig_unitful})
-  end
-  @assert sig_unitful !== sig_unitless
-
-  # First just remove all units
-  remove_unit(expr) = postwalk(expr) do x
-    @capture(x, _Symbol_U(FT_, _)) && (return FT)
-    x
-  end
-
-  attrs_unitless = map(remove_unit, attrs)
-  unitless = quote
-    struct $sig_unitless
-      $(attrs_unitless...)
+    # Rename the struct
+    local sig_unitless, sig_unitful, name, union, p1, p2
+    if @capture(sig, name_{p1__} <: sname_{p2__})
+      lname = Symbol(:L, name)
+      uname = Symbol(:U, name)
+      sig_unitless = :($lname{$(p1...)} <: $sname{$(p2...)})
+      sig_unitful  = :($uname{$(p1...)} <: $sname{$(p2...)})
+      union = :($name{$(p1...)} = Union{$lname{$(p1...)}, $uname{$(p1...)}})
+    elseif @capture(sig, name_{p1__} <: sname_)
+      p2 = Any[]
+      lname = Symbol(:L, name)
+      uname = Symbol(:U, name)
+      sig_unitless = :($lname{$(p1...)} <: $sname)
+      sig_unitful  = :($uname{$(p1...)} <: $sname)
+      union = :($name{$(p1...)} = Union{$lname{$(p1...)}, $uname{$(p1...)}})
+    elseif @capture(sig, name_{p1__})
+      lname = Symbol(:L, name)
+      uname = Symbol(:U, name)
+      sig_unitless = :($lname{$(p1...)})
+      sig_unitful  = :($uname{$(p1...)})
+      union = :($name{$(p1...)} = Union{$lname{$(p1...)}, $uname{$(p1...)}})
+    elseif @capture(sig, name_)
+      lname = Symbol(:L, name)
+      uname = Symbol(:U, name)
+      p1, p2 = Any[], Any[]
+      sig_unitless = lname
+      sig_unitful  = uname
+      union = :($name = Union{$sig_unitless, $sig_unitful})
     end
+    @assert sig_unitful !== sig_unitless
+
+    attrs_unitless, attrs_unitful = split_U(attrs)
+
+    # First just remove all units
+    unitless = quote
+      struct $sig_unitless
+        $(attrs_unitless...)
+      end
+    end
+
+    # Now make a version with unit annotations
+    unitful = quote
+      struct $sig_unitful
+        $(attrs_unitful...)
+      end
+    end
+
+    params_unitless = filter(x->!(x isa String), attrs_unitless)
+    params_unitful  = filter(x->!(x isa String), attrs_unitful)
+
+    # Lastly provide the constructors
+    constr_unitless = quote
+      function (::typeof($name{$(p1...)}))($(params_unitless...)) where {$(p1...)}
+        $lname{$(p1...)}($(attrs_unitless...))
+      end
+    end
+    constr_unitful = quote
+      function (::typeof($name{$(p1...)}))($(params_unitful... )) where {$(p1...)}
+        $uname{$(p1...)}($(attrs_unitful...))
+      end
+    end
+
+    return quote
+      Base.@__doc__ $unitless
+      Base.@__doc__ $unitful
+      Base.@__doc__ $union
+      Base.@__doc__ $constr_unitless
+      Base.@__doc__ $constr_unitful
+    end |> esc
+
+  elseif @capture(ex, (f_(params__) = body_) | (function f_(params__) body_ end))
+    params_unitless, params_unitful = split_U(params)
+    planet_params = names(CLIMA.PlanetParameters) #FIXME
+
+    # Careful of the generation order here!
+    body_unitless = postwalk(body) do x
+      (@capture(x, s_Symbol) && s in planet_params) || (return s)
+      Base.eval(CLIMA.PlanetParameters, s) |> getval |> ustrip
+    end
+
+    def_unitless = :($f($(params_unitless...)) = $body_unitless)
+    def_unitful  = :($f($(params_unitful... )) = $body_unitful)
+
+    q = quote
+      Base.@__doc__ $def_unitless
+                    $def_unitful
+    end |> esc
+    @show prettify(q)
+    q
   end
 
-  # Now make a version with unit annotations
-  take_unit(expr) = postwalk(expr) do x
+  error("Expected a structure or function definition for annotation.")
+end
+
+"""
+  From an array of expressions, replace all U(FT,:unitsymbol) statements with their
+  corresponding unitless, unitful variations, as a tuple of arrays.
+"""
+function split_U(exprs)
+  unitless(ex) = postwalk(ex) do x
     @capture(x, _Symbol_U(FT_, usym_)) && (return :(units($FT, $usym)))
     x
   end
-
-  attrs_unitful = map(take_unit, attrs)
-  unitful = quote
-    struct $sig_unitful
-      $(attrs_unitful...)
-    end
+  unitful(ex)  = postwalk(ex) do x
+    @capture(x, _Symbol_U(FT_, usym_)) && (return :(units($FT, $usym)))
+    x
   end
-
-  params_unitless = filter(x->!(x isa String), attrs_unitless)
-  params_unitful  = filter(x->!(x isa String), attrs_unitful)
-
-  # Lastly provide the constructors
-  constr_unitless = quote
-    function (::typeof($name{$(p1...)}))($(params_unitless...)) where {$(p1...)}
-      $lname{$(p1...)}($(attrs_unitless...))
-    end
-  end
-  constr_unitful = quote
-    function (::typeof($name{$(p1...)}))($(params_unitful... )) where {$(p1...)}
-      $uname{$(p1...)}($(attrs_unitful...))
-    end
-  end
-
-  return quote
-    Base.@__doc__ $unitless
-    Base.@__doc__ $unitful
-    Base.@__doc__ $union
-    Base.@__doc__ $constr_unitless
-    Base.@__doc__ $constr_unitful
-  end |> esc
+  (map(unitless, exprs), map(unitful, exprs))
 end
 
 # Quick test
 @uaware struct Foo{FT}
   x::U(FT,:massflux)
 end
-#
-# Foo{Float64}(1.0e0u"kg/m^2/s")
+
+@uaware function foo(x::U(FT,:massflux)) where {FT<:AbstractFloat}
+  R_d
+end
