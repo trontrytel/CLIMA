89a56cc4640a90b2e9dbdaedee076a67f714efd3
Anna Jaruga <ajaruga@caltech.edu>
2020-03-09 22:00:18 -0700

    rewrite warm-rain micro to have similar structure for ice

--- src/Atmos/Parameterizations/CloudPhysics/Microphysics.jl
+++ src/Atmos/Parameterizations/CloudPhysics/Microphysics.jl
@@ -15,28 +15,47 @@ using CLIMA.PlanetParameters
 using ..MicrophysicsParameters
 
 # rain fall speed
-export terminal_velocity_single_drop_coeff
+export c_velo
 export terminal_velocity
+
+# rain size distribution parameter
+export lambda
 
 # rates of conversion between microphysics categories
 export conv_q_vap_to_q_liq
+export conv_q_vap_to_q_ice
 export conv_q_liq_to_q_rai_acnv
 export conv_q_liq_to_q_rai_accr
 export conv_q_rai_to_q_vap
 
 """
-    terminal_velocity_single_drop_coeff(ρ)
+    c_velo(ρ)
 
 where:
   - `ρ` is the density of air
 
 Returns the proportionality coefficient between terminal velocity of an
-individual water drop and the square root of its radius * g.
+individual water drop and the square root of its radius.
 """
-function terminal_velocity_single_drop_coeff(ρ::FT) where {FT<:Real}
+function c_velo(ρ::FT) where {FT<:Real}
 
-    # terminal_vel_of_individual_drop = v_drop_coeff * (g * drop_radius)^(1/2)
-    return sqrt(FT(8/3) / C_drag * (ρ_cloud_liq / ρ - FT(1)))
+    # terminal_vel_of_individual_drop = c_velo * drop_radius^(1/2)
+    return sqrt(grav * FT(8/3) / C_drag * (ρ_cloud_liq / ρ - FT(1)))
+end
+
+"""
+    lambda(q_rai, ρ)
+
+where:
+  - `ρ`     - the density of air
+  - `q_rai` - rain water specific humidity
+Returns the coefficient in the exponent of the assumed size distribution of
+rain drops.
+"""
+function lambda(q_rai::FT, ρ::FT) where {FT<:Real}
+
+    return
+      (c_mass * MP_n_0 * gamma(p_mass + FT(1)) / ρ / q_rai)^FT(1/(p_mass + 1))
 end
 
 """
@@ -51,16 +70,10 @@ Marshall Palmer 1948 distribution of rain drops.
 """
 function terminal_velocity(q_rai::FT, ρ::FT) where {FT<:Real}
 
-    v_c = terminal_velocity_single_drop_coeff(ρ)
+    return c_velo(ρ) * lambda(q_rai, ρ)^(-p_velo)
+           * gamma(p_velo + p_mass + FT(1)) / gamma(p_mass + FT(1))
 
-    # gamma(9/2)
-    gamma_9_2 = FT(11.631728396567448)
-
-    lambda::FT = (FT(8) * π * ρ_cloud_liq * MP_n_0 / ρ / q_rai)^FT(1/4)
-
-    return gamma_9_2 * v_c / FT(6) * sqrt(grav / lambda)
 end
-
 
 """
     conv_q_vap_to_q_liq(q_sat, q)
@@ -76,15 +89,25 @@ constant timescale.
 function conv_q_vap_to_q_liq(q_sat::PhasePartition{FT},
                              q::PhasePartition{FT}) where {FT<:Real}
 
-  if q_sat.ice != FT(0)
-    error("1-moment bulk microphysics is not defined for snow/ice")
-    #This should be the q_ice tendency due to sublimation/resublimation.
-    #src_q_ice = (q_sat.ice - q.ice) / τ_subl_resubl
-  end
-
-  return (q_sat.liq - q.liq) / τ_cond_evap
+    return (q_sat.liq - q.liq) / τ_cond_evap_liq
 end
 
+"""
+    conv_q_vap_to_q_ice(q_sat, q)
+
+where:
+- `q_sat` - PhasePartition at equilibrium
+- `q`     - current PhasePartition
+
+Returns the q_ice tendency due to sublimation/resublimation.
+The tendency is obtained assuming a relaxation to equilibrium with
+constant timescale.
+"""
+function conv_q_vap_to_q_ice(q_sat::PhasePartition{FT},
+                             q::PhasePartition{FT}) where {FT<:Real}
+
+    return (q_sat.ice - q.ice) / τ_cond_evap_ice
+end
 
 """
     conv_q_liq_to_q_rai_acnv(q_liq)
@@ -114,17 +137,9 @@ and rain drops (accretion) parametrized following Kessler 1995.
 """
 function conv_q_liq_to_q_rai_accr(q_liq::FT, q_rai::FT, ρ::FT) where {FT<:Real}
 
-  # terminal_vel_of_individual_drop = v_drop_coeff * drop_radius^(1/2)
-  v_c = terminal_velocity_single_drop_coeff(ρ)
-
-  #gamma(7/2)
-  gamma_7_2 = FT(3.3233509704478426)
-
-  accr_coeff::FT = gamma_7_2 * FT(8)^FT(-7/8) * π^FT(1/8) * v_c * E_col *
-                   (ρ / ρ_cloud_liq)^FT(7/8)
-
-  return accr_coeff * FT(MP_n_0)^FT(1/8) * sqrt(FT(grav)) *
-         q_liq * q_rai^FT(7/8)
+    return MP_n_0 * c_area * c_velo * q_liq * E_col
+           * gamma(p_area + p_velo + FT(1))
+           * lambda(q_rai, ρ)^(-p_area - p_velo -FT(1))
 end
 
 """
@@ -143,29 +158,26 @@ Smolarkiewicz and Grabowski 1996.
 function conv_q_rai_to_q_vap(qr::FT, q::PhasePartition{FT},
                              T::FT, p::FT, ρ::FT) where {FT<:Real}
 
-  qv_sat = q_vap_saturation(T, ρ, q)
-  q_v = q.tot - q.liq - q.ice
-  S = q_v/qv_sat - 1
+    qv_sat = q_vap_saturation(T, ρ, q)
+    q_v = q.tot - q.liq - q.ice
+    S = q_v/qv_sat - FT(1)
 
-  L = latent_heat_vapor(T)
-  p_vs = saturation_vapor_pressure(T, Liquid())
-  G::FT = FT(1) / (
-            L / K_therm / T * (L / R_v / T - FT(1)) + R_v * T / D_vapor / p_vs
-          )
+    L = latent_heat_vapor(T)
+    p_vs = saturation_vapor_pressure(T, Liquid())
+    G::FT = FT(1) / (
+              L / K_therm / T * (L / R_v / T - FT(1))
+              + R_v * T / D_vapor / p_vs
+            )
 
-  # gamma(11/4)
-  gamma_11_4 = FT(1.6083594219855457)
-  N_Sc::FT = ν_air / D_vapor
-  v_c = terminal_velocity_single_drop_coeff(ρ)
-
-  av::FT = sqrt(2 * π) * a_vent * sqrt(ρ / ρ_cloud_liq)
-  bv::FT = FT(2)^FT(7/16) * gamma_11_4 * π^FT(5/16) * b_vent * (N_Sc)^FT(1/3) *
-       sqrt(v_c) * (ρ / ρ_cloud_liq)^FT(11/16)
-
-  F::FT = av * sqrt(qr) +
-          bv * grav^FT(1/4) / (MP_n_0)^FT(3/16) / sqrt(ν_air) * qr^FT(11/16)
-
-  return S * F * G * sqrt(MP_n_0) / ρ
+    return c_mass * p_mass * S * G * MP_n_0 / ρ / ρ_cloud_liq
+           * (
+               a_vent * lambda(q_rai, ρ)^(-p_mass + FT(1))
+               * gamma(p_mass - FT(1))
+               +
+               b_vent * N_Sc^FT(1/3) * (FT(2) * c_velo / ν_air
+               * lambda(q_rai, ρ)^FT(-2*p_mass - p_velo + 1))^FT(1/2)
+               * gamma(FT(2*p_mass + p_velo - 1)/FT(2))
+             )
 end
 
 end #module Microphysics.jl
--- src/Atmos/Parameterizations/CloudPhysics/MicrophysicsParameters.jl
+++ src/Atmos/Parameterizations/CloudPhysics/MicrophysicsParameters.jl
@@ -5,21 +5,35 @@ Module containing 1-moment bulk microphysics parameters.
 """
 module MicrophysicsParameters
 using CLIMA.ParametersType
+using CLIMA.PlanetParameters
 
-@exportparameter MP_n_0          8e6 * 2         "Marshall-Palmer distribution n_0 coeff [1/m4]"
-@exportparameter C_drag          0.55            "drag coefficient for rain drops [-]"
+# liquid spherical rain drops:
+# mass(radius)     = c_mass      * r^p_mass
+# velocity(radius) = c_velo(rho) * r^p_velo
+# area(radius)     = c_area      * r^p_area
 
-@exportparameter τ_cond_evap     10              "condensation/evaporation timescale [s]"
+@exportparameter c_mass 4/3. * π * ρ_cloud_liq "coefficient in mass(radius)"
+@exportparameter p_mass 3 "exponent in mass(radius)"
+@exportparameter p_velo 0.5 "exponent in velocity(radius)"
+@exportparameter c_area π "coefficient in area(radius) relation"
+@exportparameter p_area 2 "exponent in area(radius) relation"
 
-@exportparameter q_liq_threshold 5e-4            "autoconversion threshold [-]  ∈(0.5, 1) * 1e-3 "
-@exportparameter τ_acnv          1e3             "autoconversion timescale [s]  ∈(1e3, 1e4) "
+@exportparameter MP_n_0 8e6 * 2 "Marshall-Palmer distribution n_0 coeff [1/m4]"
+@exportparameter C_drag 0.55 "drag coefficient for rain drops [-]"
 
-@exportparameter E_col           0.8             "collision efficiency [-]"
+@exportparameter τ_cond_evap_liq 10 "condensation/evaporation timescale [s]"
+@exportparameter τ_cond_evap_ice 10 "sublimation/resublimation timescale [s]"
 
-@exportparameter a_vent          1.5             "ventilation factor coefficient [-]"
-@exportparameter b_vent          0.53            "ventilation factor coefficient [-]"
-@exportparameter K_therm         2.4e-2          "thermal conductivity of air [J/m/s/K] "
-@exportparameter D_vapor         2.26e-5         "diffusivity of water vapor [m2/s]"
-@exportparameter ν_air           1.6e-5          "kinematic viscosity of air [m2/s]"
-@exportparameter N_Sc            ν_air/D_vapor   "Schmidt number [-]"
+@exportparameter τ_acnv 1e3 "autoconversion timescale [s]  ∈(1e3, 1e4) "
+@exportparameter q_liq_threshold 5e-4 "autoconv. threshold [-] ∈(.5, 1) * 1e-3"
+
+@exportparameter E_col 0.8 "collision efficiency [-]"
+
+@exportparameter a_vent 1.5 "ventilation factor coefficient [-]"
+@exportparameter b_vent 0.53 "ventilation factor coefficient [-]"
+@exportparameter K_therm 2.4e-2 "thermal conductivity of air [J/m/s/K] "
+@exportparameter D_vapor 2.26e-5 "diffusivity of water vapor [m2/s]"
+@exportparameter ν_air 1.6e-5 "kinematic viscosity of air [m2/s]"
+@exportparameter N_Sc ν_air/D_vapor "Schmidt number [-]"
+
 end
